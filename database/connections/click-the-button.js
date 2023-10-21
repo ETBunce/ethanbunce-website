41ba1af544440fecab1bb7770b04142ca132c62b
ETBunce <etbunce@gmail.com>
2023-10-20 22:41:34 -0600

    removed console log exposing connection string

--- database/connections/click-the-button.js
+++ database/connections/click-the-button.js
@@ -2,10 +2,7 @@ import mongoose, { mongo } from "mongoose";
 
 const url = process.env.MONGODB_CONNECTION_STRING + 'ClickTheButton' || ''
 
-console.log('ClickTheButton connection string: ' + url);
-
 const conn = await mongoose.createConnection(url).asPromise();
-console.log('ClickTheButton connection readyState: ' + conn.readyState);
 
 conn.model('Count', mongoose.Schema({
     count: Number
